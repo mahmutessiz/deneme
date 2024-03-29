7227288d761720a214d361cf37d97c546e770c23
Mahmut Essiz <mahmutessiz71@gmail.com>
2024-03-29 12:39:56 +0300

    try blurview

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,7 +1,8 @@
+import { BlurView } from '@react-native-community/blur';
 import TaskSelect from 'components/taskSelect';
-import { Text, View } from 'react-native';
+import React, { Component } from 'react';
+import { Text, View, Image, StyleSheet } from 'react-native';
 import { useStyles } from 'react-native-unistyles';
-
 export default function TabOneScreen() {
   const { theme } = useStyles();
 
@@ -11,6 +12,35 @@ export default function TabOneScreen() {
         <Text style={theme.components.title}>Achivements</Text>
       </View>
       <TaskSelect />
+      <View style={styles.container}>
+        <Image
+          key="blurryImage"
+          source={{ uri: 'https://picsum.photos/500/300' }}
+          style={styles.absolute}
+        />
+        <Text style={styles.absolute}>Hi, I am some blurred text</Text>
+        {/* in terms of positioning and zIndex-ing everything before the BlurView will be blurred */}
+        <BlurView
+          style={styles.absolute}
+          blurType="light"
+          blurAmount={10}
+          reducedTransparencyFallbackColor="white"
+        />
+        <Text>I'm the non blurred text because I got rendered on top of the BlurView</Text>
+      </View>
     </>
   );
 }
+const styles = StyleSheet.create({
+  container: {
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  absolute: {
+    position: 'absolute',
+    top: 0,
+    left: 0,
+    bottom: 0,
+    right: 0,
+  },
+});
